1d08120dbe02f1956cebbdb52e89d1310da16547
gitplus <gitplus@witplus.com>
2023-04-13 17:44:21 +0900

    link 수정

--- src/App.js
+++ src/App.js
@@ -10,15 +10,15 @@ function App() {
     <div>
         <header>
             <nav>
-                <Link to="/">Home</Link>&nbsp;&nbsp;
+                <Link to="/MovieApp">Home</Link>&nbsp;&nbsp;
             </nav>
 
             <Search />
         </header>
 
         <Routes>
-            <Route path="/" element={<MovieList />}></Route>
-            <Route path="/view/:key" element={<MovieView />}></Route>
+            <Route path="/MovieApp" element={<MovieList />}></Route>
+            <Route path="/MovieApp/view/:key" element={<MovieView />}></Route>
         </Routes>
     </div>
     )
--- src/pages/MovieListForm.js
+++ src/pages/MovieListForm.js
@@ -9,7 +9,7 @@ function MovieListForm({ movieList }) {
             <ul>
             { movieList && movieList.map( item => (
                 <li key={item.movieCd}>
-                    <Link to="/view/:key" state={item.movieCd}>{item.movieNm}</Link>
+                    <Link to="/MovieApp/view/:key" state={item.movieCd}>{item.movieNm}</Link>
                 </li>
             )) }
             </ul>
